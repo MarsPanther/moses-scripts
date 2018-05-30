13203d1a748ac8ec7f5208b351901072c4651716
adtsegaye <adtsegaye@gmail.com>
2018-05-30 11:38:34 +0300

    tnt run for translation

--- bi_factored_exp.sh
+++ bi_factored_exp.sh
@@ -3,7 +3,7 @@
 LANG1="en"
 LANG2="am"
 START_DIR="$HOME/Desktop/data"
-EXP_DIR="Factored-exp-$LANG1-$LANG2-4"
+EXP_DIR="Factored-Trigram-$LANG1-$LANG2"
 
 mkdir $HOME/$EXP_DIR
 mkdir $HOME/$EXP_DIR/corpus
@@ -11,7 +11,7 @@ mkdir $HOME/$EXP_DIR/working
 mkdir $HOME/$EXP_DIR/lm
 mkdir $HOME/$EXP_DIR/pre_process
 
-SRC="$START_DIR/$LANG1"_"$LANG2/"
+SRC="$START_DIR/tree_$LANG1"_"$LANG2/"
 SELECTOR_SCRIPS="$START_DIR/scripts/selector_test_tune.py"
 COMBINE_FILES="$START_DIR/scripts/combine.py"
 MOSSES_TOKEN="$HOME/mosesdecoder/scripts/tokenizer/tokenizer.perl"
@@ -30,87 +30,20 @@ EXTRACT_TAGS="$START_DIR/scripts/extract_tag.py"
 LM="$HOME/$EXP_DIR/lm"
 WORKING="$HOME/$EXP_DIR/working"
 
+PREV_SRC="$HOME/Factored-Trigram-$LANG2-$LANG1"
 
-cp "$SRC$LANG1.txt"  "$PreProcess/"
-cp "$SRC$LANG2.txt"  "$PreProcess/"
+cp -R "$PREV_SRC/corpus/." "$HOME/$EXP_DIR/corpus"
+cp -R "$PREV_SRC/lm/." "$HOME/$EXP_DIR/lm"
 
-cd $PreProcess
-python3  "$SELECTOR_SCRIPS" $LANG1 $LANG2
+rm -rf "$HOME/$EXP_DIR/blue_$LANG2_$LANG1.txt"
 
-# # English tokenization and truecasing
-"$MOSSES_TOKEN"  -l en < "$PreProcess/train.$LANG1" > "$PreProcess/train.$LANG1.tok.$LANG1"
-"$MOSSES_TRUE_CASE_MOD"  --model "$PreProcess/truecase-model.$LANG1" --corpus "$PreProcess/train.$LANG1.tok.$LANG1"
-"$MOSSES_TRUE_CASE_BULD"  --model "$PreProcess/truecase-model.$LANG1" < "$PreProcess/train.$LANG1.tok.$LANG1" > "$PreProcess/train.true.$LANG1"
-
-
-"$MOSSES_TOKEN" -l en < "$PreProcess/tune.$LANG1" > "$PreProcess/tune.$LANG1.tok.$LANG1"
-"$MOSSES_TRUE_CASE_MOD"  --model "$PreProcess/truecase-model2.$LANG1" --corpus "$PreProcess/tune.$LANG1.tok.$LANG1"
-"$MOSSES_TRUE_CASE_BULD"  --model "$PreProcess/truecase-model2.$LANG1" < "$PreProcess/tune.$LANG1.tok.$LANG1" > "$PreProcess/tune.true.$LANG1"
+rm -rf "$HOME/$EXP_DIR/corpus/train.clean_tagged.0-0.$LANG1"
+rm -rf "$HOME/$EXP_DIR/corpus/train.clean_tagged.0-0.$LANG2"
+rm -rf "$HOME/$EXP_DIR/corpus/test.translated.$LANG1"
 
 
-"$MOSSES_TOKEN" -l en < "$PreProcess/test.$LANG1" > "$PreProcess/test.$LANG1.tok.$LANG1"
-"$MOSSES_TRUE_CASE_MOD"  --model "$PreProcess/truecase-model3.$LANG1" --corpus "$PreProcess/test.$LANG1.tok.$LANG1"
-"$MOSSES_TRUE_CASE_BULD"  --model "$PreProcess/truecase-model3.$LANG1" < "$PreProcess/test.$LANG1.tok.$LANG1" > "$PreProcess/test.true.$LANG1"
-
-#Return to 
-# # Amharic tokenization and character normalaization
-cd "$START_DIR/scripts/"
-
-python2 "$SPACE_NORM" "$PreProcess/train."$LANG2
-python2 "$SPACE_NORM" "$PreProcess/tune."$LANG2
-python2 "$SPACE_NORM" "$PreProcess/test."$LANG2
-
-python2 "$CHAR_NORM" "$PreProcess/train."$LANG2
-python2 "$CHAR_NORM" "$PreProcess/tune."$LANG2
-python2 "$CHAR_NORM" "$PreProcess/test."$LANG2
-
-mv "$PreProcess/train.$LANG2" "$PreProcess/train.true."$LANG2 
-mv "$PreProcess/tune.$LANG2" "$PreProcess/tune.true."$LANG2 
-mv "$PreProcess/test.$LANG2" "$PreProcess/test.true."$LANG2
 
 
-"$MOSSES_CLEAN" "$PreProcess/train.true" $LANG2 $LANG1 "$PreProcess/train.clean" 1 80
-
-cp "$PreProcess/train.clean."$LANG1 "$PreProcess/train.clean."$LANG2 "$CORPUS"
-cp "$PreProcess/tune.true."$LANG1 "$PreProcess/tune.true."$LANG2 "$CORPUS"
-cp "$PreProcess/test.true."$LANG1 "$PreProcess/test.true."$LANG2 "$CORPUS"
-
-# # #Tagging with best model yet
-
-cd $CORPUS
-python3 "$ENG_TAGGER" "$CORPUS/train.clean.$LANG1"
-python3 "$ENG_TAGGER" "$CORPUS/tune.true.$LANG1"
-python3 "$AMH_TAGGER" "$CORPUS/train.clean.$LANG2"
-python3 "$AMH_TAGGER" "$CORPUS/tune.true.$LANG2"
-
-
-# # # language model creation
-cd $LM
-python3 "$COMBINE_FILES" "$PreProcess/train.true."$LANG1  "$PreProcess/tune.true."$LANG1 "$LM/combined_lm.$LANG1"
-python3 "$COMBINE_FILES" "$PreProcess/train.true."$LANG2  "$PreProcess/tune.true."$LANG2 "$LM/combined_lm.$LANG2"
-
-python3 "$COMBINE_FILES" "$CORPUS/train.clean_tagged."$LANG1  "$CORPUS/tune.true_tagged."$LANG1 "$LM/combined_tagged_lm.$LANG1"
-python3 "$COMBINE_FILES" "$CORPUS/train.clean_tagged."$LANG2  "$CORPUS/tune.true_tagged."$LANG2 "$LM/combined_tagged_lm.$LANG2"
-
-python3 "$EXTRACT_TAGS" "$LM/combined_tagged_lm.$LANG1"
-python3 "$EXTRACT_TAGS" "$LM/combined_tagged_lm.$LANG2"
-
-
-python3 "$COMBINE_FILES" "combined_tagged_lm_tag_sets.$LANG1"  "combined_tagged_lm_tag_sets.$LANG1" "combined_tagged_lm_tag_sets_double.$LANG1"
-python3 "$COMBINE_FILES" "combined_tagged_lm_tag_sets.$LANG2"  "combined_tagged_lm_tag_sets.$LANG2" "combined_tagged_lm_tag_sets_double.$LANG2"
-
-# lang model
-$HOME/mosesdecoder/bin/lmplz -o 4 --interpolate_unigrams 0 --discount_fallback <"$LM/combined_lm.$LANG1" >"$LM/surface.arpa.$LANG1"
-$HOME/mosesdecoder/bin/lmplz -o 4 --interpolate_unigrams 0 --discount_fallback <"$LM/combined_lm.$LANG2" >"$LM/surface.arpa.$LANG2"
-$HOME/mosesdecoder/bin/lmplz -o 4 --interpolate_unigrams 0 --discount_fallback <"$LM/combined_tagged_lm_tag_sets_double.$LANG1" >"$LM/pos.arpa.$LANG1"
-$HOME/mosesdecoder/bin/lmplz -o 4 --interpolate_unigrams 0 --discount_fallback <"$LM/combined_tagged_lm_tag_sets_double.$LANG2" >"$LM/pos.arpa.$LANG2"
-
-# # # ####Binarize LM-------------------------------------------------------------------------------------
-
-"$HOME/mosesdecoder/bin/build_binary" "$LM/surface.arpa.$LANG1" "$LM/surface.blm.$LANG1"
-"$HOME/mosesdecoder/bin/build_binary" "$LM/surface.arpa.$LANG2" "$LM/surface.blm.$LANG2"
-"$HOME/mosesdecoder/bin/build_binary" "$LM/pos.arpa.$LANG1" "$LM/pos.blm.$LANG1"
-"$HOME/mosesdecoder/bin/build_binary" "$LM/pos.arpa.$LANG1" "$LM/pos.blm.$LANG2"
 
 # ##Translation Model-------------------------------------------------------------------------
 
@@ -120,8 +53,8 @@ cd $WORKING
 -root-dir "$WORKING/train/model" \
 -corpus "$CORPUS/train.clean_tagged" \
 -f "$LANG1" -e "$LANG2" \
--lm 0:4:"$LM/surface.blm."$LANG2 \
--lm 2:4:"$LM/pos.blm."$LANG2 \
+-lm 0:3:"$LM/surface.blm."$LANG2 \
+-lm 1:3:"$LM/pos.blm."$LANG2 \
 -translation-factors 0-0,2 \
 -external-bin-dir "$HOME/mosesdecoder/tools"
 
@@ -135,7 +68,7 @@ cd $WORKING
     -rootdir "$HOME/mosesdecoder/scripts" \
     -decoder-flags '-threads 4'
 
-####Binaries-------------------------------------------------------------------------------
+# ####Binaries-------------------------------------------------------------------------------
 
 
 
@@ -151,16 +84,17 @@ cp "$WORKING/mert-work/moses.ini" "$WORKING/train/model/model"
 
 
 sed -i -e "s/PhraseDictionaryMemory/PhraseDictionaryCompact/g" "$WORKING/train/model/model/moses.ini"
-sed -i  -e "s#path=$WORKING/train/model/model/phrase-table.0-0,2.gz#path=$WORKING/train/model/model/phrase-table.0-0,2.minphr#g" "$WORKING/train/model/model/moses.ini"
+sed -i  -e "s#phrase-table.0-0,2.gz#phrase-table.0-0,2.minphr#g" "$WORKING/train/model/model/moses.ini"
 
 
 
-# # # ###bleu----------------------------------------------------------------------------------------------------------
+# # # # ###bleu----------------------------------------------------------------------------------------------------------
 cd $WORKING
 "$HOME/mosesdecoder/bin/moses" -f "$WORKING/train/model/model/moses.ini" < "$CORPUS/test.true."$LANG1 > "$CORPUS/test.translated."$LANG2
 
-# # # # # ##run blue script------------------------------------------------------------------------------------------------
-"$HOME/mosesdecoder/scripts/generic/multi-bleu.perl" -lc "$CORPUS/test.true."$LANG2 < "$CORPUS/test.translated."$LANG2 > "$CORPUS/BLUElog."$LANG1"_"$LANG2
+# # # # # # ##run blue script------------------------------------------------------------------------------------------------
+"$HOME/mosesdecoder/scripts/generic/multi-bleu.perl" -lc "$CORPUS/test.true."$LANG2 < "$CORPUS/test.translated."$LANG2 > "$HOME/$EXP_DIR/blue_$LANG2_$LANG1.txt"
 
 #continue to bidirectional
 echo "writting BLUE for EXP-$LANG1-$LANG2 Done!! "
+echo "Success !!! Two Experiment done"
--- factored_exp.sh
+++ factored_exp.sh
@@ -5,11 +5,11 @@ LANG2="am"
 START_DIR="$HOME/Desktop/data"
 EXP_DIR="Factored-Trigram-$LANG2-$LANG1"
 
-# mkdir $HOME/$EXP_DIR
-# mkdir $HOME/$EXP_DIR/corpus
-# mkdir $HOME/$EXP_DIR/working
-# mkdir $HOME/$EXP_DIR/lm
-# mkdir $HOME/$EXP_DIR/pre_process
+mkdir $HOME/$EXP_DIR
+mkdir $HOME/$EXP_DIR/corpus
+mkdir $HOME/$EXP_DIR/working
+mkdir $HOME/$EXP_DIR/lm
+mkdir $HOME/$EXP_DIR/pre_process
 
 SRC="$START_DIR/$LANG1"_"$LANG2/"
 SELECTOR_SCRIPS="$START_DIR/scripts/selector_test_tune.py"
@@ -31,144 +31,144 @@ LM="$HOME/$EXP_DIR/lm"
 WORKING="$HOME/$EXP_DIR/working"
 
 
-# cp "$SRC$LANG1.txt"  "$PreProcess/"
-# cp "$SRC$LANG2.txt"  "$PreProcess/"
+cp "$SRC$LANG1.txt"  "$PreProcess/"
+cp "$SRC$LANG2.txt"  "$PreProcess/"
 
-# cd $PreProcess
-# python3  "$SELECTOR_SCRIPS" $LANG1 $LANG2
+cd $PreProcess
+python3  "$SELECTOR_SCRIPS" $LANG1 $LANG2
 
-# # # English tokenization and truecasing
-# "$MOSSES_TOKEN"  -l en < "$PreProcess/train.$LANG1" > "$PreProcess/train.$LANG1.tok.$LANG1"
-# "$MOSSES_TRUE_CASE_MOD"  --model "$PreProcess/truecase-model.$LANG1" --corpus "$PreProcess/train.$LANG1.tok.$LANG1"
-# "$MOSSES_TRUE_CASE_BULD"  --model "$PreProcess/truecase-model.$LANG1" < "$PreProcess/train.$LANG1.tok.$LANG1" > "$PreProcess/train.true.$LANG1"
-
-
-# "$MOSSES_TOKEN" -l en < "$PreProcess/tune.$LANG1" > "$PreProcess/tune.$LANG1.tok.$LANG1"
-# "$MOSSES_TRUE_CASE_MOD"  --model "$PreProcess/truecase-model2.$LANG1" --corpus "$PreProcess/tune.$LANG1.tok.$LANG1"
-# "$MOSSES_TRUE_CASE_BULD"  --model "$PreProcess/truecase-model2.$LANG1" < "$PreProcess/tune.$LANG1.tok.$LANG1" > "$PreProcess/tune.true.$LANG1"
+# # English tokenization and truecasing
+"$MOSSES_TOKEN"  -l en < "$PreProcess/train.$LANG1" > "$PreProcess/train.$LANG1.tok.$LANG1"
+"$MOSSES_TRUE_CASE_MOD"  --model "$PreProcess/truecase-model.$LANG1" --corpus "$PreProcess/train.$LANG1.tok.$LANG1"
+"$MOSSES_TRUE_CASE_BULD"  --model "$PreProcess/truecase-model.$LANG1" < "$PreProcess/train.$LANG1.tok.$LANG1" > "$PreProcess/train.true.$LANG1"
 
 
-# "$MOSSES_TOKEN" -l en < "$PreProcess/test.$LANG1" > "$PreProcess/test.$LANG1.tok.$LANG1"
-# "$MOSSES_TRUE_CASE_MOD"  --model "$PreProcess/truecase-model3.$LANG1" --corpus "$PreProcess/test.$LANG1.tok.$LANG1"
-# "$MOSSES_TRUE_CASE_BULD"  --model "$PreProcess/truecase-model3.$LANG1" < "$PreProcess/test.$LANG1.tok.$LANG1" > "$PreProcess/test.true.$LANG1"
-
-# #Return to 
-# # # Amharic tokenization and character normalaization
-# cd "$START_DIR/scripts/"
-
-# python2 "$SPACE_NORM" "$PreProcess/train."$LANG2
-# python2 "$SPACE_NORM" "$PreProcess/tune."$LANG2
-# python2 "$SPACE_NORM" "$PreProcess/test."$LANG2
-
-# python2 "$CHAR_NORM" "$PreProcess/train."$LANG2
-# python2 "$CHAR_NORM" "$PreProcess/tune."$LANG2
-# python2 "$CHAR_NORM" "$PreProcess/test."$LANG2
-
-# mv "$PreProcess/train.$LANG2" "$PreProcess/train.true."$LANG2 
-# mv "$PreProcess/tune.$LANG2" "$PreProcess/tune.true."$LANG2 
-# mv "$PreProcess/test.$LANG2" "$PreProcess/test.true."$LANG2
+"$MOSSES_TOKEN" -l en < "$PreProcess/tune.$LANG1" > "$PreProcess/tune.$LANG1.tok.$LANG1"
+"$MOSSES_TRUE_CASE_MOD"  --model "$PreProcess/truecase-model2.$LANG1" --corpus "$PreProcess/tune.$LANG1.tok.$LANG1"
+"$MOSSES_TRUE_CASE_BULD"  --model "$PreProcess/truecase-model2.$LANG1" < "$PreProcess/tune.$LANG1.tok.$LANG1" > "$PreProcess/tune.true.$LANG1"
 
 
-# "$MOSSES_CLEAN" "$PreProcess/train.true" $LANG2 $LANG1 "$PreProcess/train.clean" 1 80
+"$MOSSES_TOKEN" -l en < "$PreProcess/test.$LANG1" > "$PreProcess/test.$LANG1.tok.$LANG1"
+"$MOSSES_TRUE_CASE_MOD"  --model "$PreProcess/truecase-model3.$LANG1" --corpus "$PreProcess/test.$LANG1.tok.$LANG1"
+"$MOSSES_TRUE_CASE_BULD"  --model "$PreProcess/truecase-model3.$LANG1" < "$PreProcess/test.$LANG1.tok.$LANG1" > "$PreProcess/test.true.$LANG1"
 
-# cp "$PreProcess/train.clean."$LANG1 "$PreProcess/train.clean."$LANG2 "$CORPUS"
-# cp "$PreProcess/tune.true."$LANG1 "$PreProcess/tune.true."$LANG2 "$CORPUS"
-# cp "$PreProcess/test.true."$LANG1 "$PreProcess/test.true."$LANG2 "$CORPUS"
+#Return to 
+# # Amharic tokenization and character normalaization
+cd "$START_DIR/scripts/"
+
+python2 "$SPACE_NORM" "$PreProcess/train."$LANG2
+python2 "$SPACE_NORM" "$PreProcess/tune."$LANG2
+python2 "$SPACE_NORM" "$PreProcess/test."$LANG2
+
+python2 "$CHAR_NORM" "$PreProcess/train."$LANG2
+python2 "$CHAR_NORM" "$PreProcess/tune."$LANG2
+python2 "$CHAR_NORM" "$PreProcess/test."$LANG2
+
+mv "$PreProcess/train.$LANG2" "$PreProcess/train.true."$LANG2 
+mv "$PreProcess/tune.$LANG2" "$PreProcess/tune.true."$LANG2 
+mv "$PreProcess/test.$LANG2" "$PreProcess/test.true."$LANG2
+
+
+"$MOSSES_CLEAN" "$PreProcess/train.true" $LANG2 $LANG1 "$PreProcess/train.clean" 1 80
+
+cp "$PreProcess/train.clean."$LANG1 "$PreProcess/train.clean."$LANG2 "$CORPUS"
+cp "$PreProcess/tune.true."$LANG1 "$PreProcess/tune.true."$LANG2 "$CORPUS"
+cp "$PreProcess/test.true."$LANG1 "$PreProcess/test.true."$LANG2 "$CORPUS"
 
 # # # #Tagging with best model yet
 
-# cd $CORPUS
-# python3 "$ENG_TAGGER" "$CORPUS/train.clean.$LANG1"
-# python3 "$ENG_TAGGER" "$CORPUS/tune.true.$LANG1"
-# python3 "$AMH_TAGGER" "$CORPUS/train.clean.$LANG2"
-# python3 "$AMH_TAGGER" "$CORPUS/tune.true.$LANG2"
+cd $CORPUS
+python3 "$ENG_TAGGER" "$CORPUS/train.clean.$LANG1"
+python3 "$ENG_TAGGER" "$CORPUS/tune.true.$LANG1"
+python3 "$AMH_TAGGER" "$CORPUS/train.clean.$LANG2"
+python3 "$AMH_TAGGER" "$CORPUS/tune.true.$LANG2"
 
 
 # # # # language model creation
-# cd $LM
-# python3 "$COMBINE_FILES" "$PreProcess/train.true."$LANG1  "$PreProcess/tune.true."$LANG1 "$LM/combined_lm.$LANG1"
-# python3 "$COMBINE_FILES" "$PreProcess/train.true."$LANG2  "$PreProcess/tune.true."$LANG2 "$LM/combined_lm.$LANG2"
+cd $LM
+python3 "$COMBINE_FILES" "$PreProcess/train.true."$LANG1  "$PreProcess/tune.true."$LANG1 "$LM/combined_lm.$LANG1"
+python3 "$COMBINE_FILES" "$PreProcess/train.true."$LANG2  "$PreProcess/tune.true."$LANG2 "$LM/combined_lm.$LANG2"
 
-# python3 "$COMBINE_FILES" "$CORPUS/train.clean_tagged."$LANG1  "$CORPUS/tune.true_tagged."$LANG1 "$LM/combined_tagged_lm.$LANG1"
-# python3 "$COMBINE_FILES" "$CORPUS/train.clean_tagged."$LANG2  "$CORPUS/tune.true_tagged."$LANG2 "$LM/combined_tagged_lm.$LANG2"
+python3 "$COMBINE_FILES" "$CORPUS/train.clean_tagged."$LANG1  "$CORPUS/tune.true_tagged."$LANG1 "$LM/combined_tagged_lm.$LANG1"
+python3 "$COMBINE_FILES" "$CORPUS/train.clean_tagged."$LANG2  "$CORPUS/tune.true_tagged."$LANG2 "$LM/combined_tagged_lm.$LANG2"
 
-# python3 "$EXTRACT_TAGS" "$LM/combined_tagged_lm.$LANG1"
-# python3 "$EXTRACT_TAGS" "$LM/combined_tagged_lm.$LANG2"
+python3 "$EXTRACT_TAGS" "$LM/combined_tagged_lm.$LANG1"
+python3 "$EXTRACT_TAGS" "$LM/combined_tagged_lm.$LANG2"
 
 
-# python3 "$COMBINE_FILES" "combined_tagged_lm_tag_sets.$LANG1"  "combined_tagged_lm_tag_sets.$LANG1" "combined_tagged_lm_tag_sets_double.$LANG1"
-# python3 "$COMBINE_FILES" "combined_tagged_lm_tag_sets.$LANG2"  "combined_tagged_lm_tag_sets.$LANG2" "combined_tagged_lm_tag_sets_double.$LANG2"
+python3 "$COMBINE_FILES" "combined_tagged_lm_tag_sets.$LANG1"  "combined_tagged_lm_tag_sets.$LANG1" "combined_tagged_lm_tag_sets_double.$LANG1"
+python3 "$COMBINE_FILES" "combined_tagged_lm_tag_sets.$LANG2"  "combined_tagged_lm_tag_sets.$LANG2" "combined_tagged_lm_tag_sets_double.$LANG2"
 
-# # lang model
-# $HOME/mosesdecoder/bin/lmplz -o 4 --interpolate_unigrams 0 --discount_fallback <"$LM/combined_lm.$LANG1" >"$LM/surface.arpa.$LANG1"
-# $HOME/mosesdecoder/bin/lmplz -o 4 --interpolate_unigrams 0 --discount_fallback <"$LM/combined_lm.$LANG2" >"$LM/surface.arpa.$LANG2"
-# $HOME/mosesdecoder/bin/lmplz -o 4 --interpolate_unigrams 0 --discount_fallback <"$LM/combined_tagged_lm_tag_sets_double.$LANG1" >"$LM/pos.arpa.$LANG1"
-# $HOME/mosesdecoder/bin/lmplz -o 4 --interpolate_unigrams 0 --discount_fallback <"$LM/combined_tagged_lm_tag_sets_double.$LANG2" >"$LM/pos.arpa.$LANG2"
+# lang model
+$HOME/mosesdecoder/bin/lmplz -o 3 --interpolate_unigrams 0 --discount_fallback <"$LM/combined_lm.$LANG1" >"$LM/surface.arpa.$LANG1"
+$HOME/mosesdecoder/bin/lmplz -o 3 --interpolate_unigrams 0 --discount_fallback <"$LM/combined_lm.$LANG2" >"$LM/surface.arpa.$LANG2"
+$HOME/mosesdecoder/bin/lmplz -o 3 --interpolate_unigrams 0 --discount_fallback <"$LM/combined_tagged_lm_tag_sets_double.$LANG1" >"$LM/pos.arpa.$LANG1"
+$HOME/mosesdecoder/bin/lmplz -o 3 --interpolate_unigrams 0 --discount_fallback <"$LM/combined_tagged_lm_tag_sets_double.$LANG2" >"$LM/pos.arpa.$LANG2"
 
-# # # # ####Binarize LM-------------------------------------------------------------------------------------
+# # # ####Binarize LM-------------------------------------------------------------------------------------
 
-# "$HOME/mosesdecoder/bin/build_binary" "$LM/surface.arpa.$LANG1" "$LM/surface.blm.$LANG1"
-# "$HOME/mosesdecoder/bin/build_binary" "$LM/surface.arpa.$LANG2" "$LM/surface.blm.$LANG2"
-# "$HOME/mosesdecoder/bin/build_binary" "$LM/pos.arpa.$LANG1" "$LM/pos.blm.$LANG1"
-# "$HOME/mosesdecoder/bin/build_binary" "$LM/pos.arpa.$LANG1" "$LM/pos.blm.$LANG2"
+"$HOME/mosesdecoder/bin/build_binary" "$LM/surface.arpa.$LANG1" "$LM/surface.blm.$LANG1"
+"$HOME/mosesdecoder/bin/build_binary" "$LM/surface.arpa.$LANG2" "$LM/surface.blm.$LANG2"
+"$HOME/mosesdecoder/bin/build_binary" "$LM/pos.arpa.$LANG1" "$LM/pos.blm.$LANG1"
+"$HOME/mosesdecoder/bin/build_binary" "$LM/pos.arpa.$LANG1" "$LM/pos.blm.$LANG2"
 
 # ##Translation Model-------------------------------------------------------------------------
 
-# cd $WORKING
+cd $WORKING
 
-# "$MOSSES_TRN" -cores 4 \
-# -root-dir "$WORKING/train/model" \
-# -corpus "$CORPUS/train.clean_tagged" \
-# -f "$LANG2" -e "$LANG1" \
-# -lm 0:4:"$LM/surface.blm."$LANG1 \
-# -lm 2:4:"$LM/pos.blm."$LANG1 \
-# -translation-factors 0-0,2 \
-# -external-bin-dir "$HOME/mosesdecoder/tools"
+"$MOSSES_TRN" -cores 4 \
+-root-dir "$WORKING/train/model" \
+-corpus "$CORPUS/train.clean_tagged" \
+-f "$LANG2" -e "$LANG1" \
+-lm 0:4:"$LM/surface.blm."$LANG1 \
+-lm 2:4:"$LM/pos.blm."$LANG1 \
+-translation-factors 0-0,2 \
+-external-bin-dir "$HOME/mosesdecoder/tools"
 
 
-# cd $WORKING
+cd $WORKING
 
-# "$MOSSES_MERT" "$CORPUS/tune.true.$LANG2" "$CORPUS/tune.true.$LANG1" \
-#     "$HOME/mosesdecoder/bin/moses" \
-#     "$WORKING/train/model/model/moses.ini" \
-#     -mertdir "$HOME/mosesdecoder/bin/" \
-#     -rootdir "$HOME/mosesdecoder/scripts" \
-#     -decoder-flags '-threads 4'
+"$MOSSES_MERT" "$CORPUS/tune.true.$LANG2" "$CORPUS/tune.true.$LANG1" \
+    "$HOME/mosesdecoder/bin/moses" \
+    "$WORKING/train/model/model/moses.ini" \
+    -mertdir "$HOME/mosesdecoder/bin/" \
+    -rootdir "$HOME/mosesdecoder/scripts" \
+    -decoder-flags '-threads 4'
 
 ####Binaries-------------------------------------------------------------------------------
 
 
 
-# cd $WORKING
+cd $WORKING
 
-# rm -rf "$WORKING/train/model/model/moses.ini"
-# cp "$WORKING/mert-work/moses.ini" "$WORKING/train/model/model"
+rm -rf "$WORKING/train/model/model/moses.ini"
+cp "$WORKING/mert-work/moses.ini" "$WORKING/train/model/model"
 
-# "$HOME/mosesdecoder/bin/processPhraseTableMin" \
-# -in "$WORKING/train/model/model/phrase-table.0-0,2.gz" \
-# -nscores 4 \
-# -out "$WORKING/train/model/model/phrase-table.0-0,2.minphr"
+"$HOME/mosesdecoder/bin/processPhraseTableMin" \
+-in "$WORKING/train/model/model/phrase-table.0-0,2.gz" \
+-nscores 4 \
+-out "$WORKING/train/model/model/phrase-table.0-0,2.minphr"
 
 
-# sed -i -e "s/PhraseDictionaryMemory/PhraseDictionaryCompact/g" "$WORKING/train/model/model/moses.ini"
-# sed -i  -e "s#path=$WORKING/train/model/model/phrase-table.0-0,2.gz#path=$WORKING/train/model/model/phrase-table.0-0,2.minphr#g" "$WORKING/train/model/model/moses.ini"
+sed -i -e "s/PhraseDictionaryMemory/PhraseDictionaryCompact/g" "$WORKING/train/model/model/moses.ini"
+sed -i  -e "s#path=$WORKING/train/model/model/phrase-table.0-0,2.gz#path=$WORKING/train/model/model/phrase-table.0-0,2.minphr#g" "$WORKING/train/model/model/moses.ini"
 
 
 
 # # # # ###bleu----------------------------------------------------------------------------------------------------------
-# cd $WORKING
-# "$HOME/mosesdecoder/bin/moses" -f "$WORKING/train/model/model/moses.ini" < "$CORPUS/test.true."$LANG2 > "$CORPUS/test.translated."$LANG1
+cd $WORKING
+"$HOME/mosesdecoder/bin/moses" -f "$WORKING/train/model/model/moses.ini" < "$CORPUS/test.true."$LANG2 > "$CORPUS/test.translated."$LANG1
 
-# # # # # # ##run blue script------------------------------------------------------------------------------------------------
-# "$HOME/mosesdecoder/scripts/generic/multi-bleu.perl" -lc "$CORPUS/test.true."$LANG1 < "$CORPUS/test.translated."$LANG1 > "$CORPUS/BLUElog."$LANG2"_"$LANG1
+# # # # # ##run blue script------------------------------------------------------------------------------------------------
+"$HOME/mosesdecoder/scripts/generic/multi-bleu.perl" -lc "$CORPUS/test.true."$LANG1 < "$CORPUS/test.translated."$LANG1 > "$HOME/$EXP_DIR/blue_$LANG2_$LANG1.txt"
 
-#continue to bidirectional
-# echo "writting BLUE for EXP-$LANG2-$LANG1 Done!! "
-# echo "================================================================================"
-# echo "Starting EXP-$LANG1-$LANG2"
-# cd "$START_DIR/scripts/"
+continue to bidirectional
+echo "writting BLUE for EXP-$LANG2-$LANG1 Done!! "
+echo "================================================================================"
+echo "Starting EXP-$LANG1-$LANG2"
+cd "$START_DIR/scripts/"
 
-# bash bi_factored_exp.sh
+bash bi_factored_exp.sh
 
-# echo "writting BLUE for EXP-$LANG1-$LANG2 Done!! "
-# echo "Success !!! Two Experiment done"
+echo "writting BLUE for EXP-$LANG1-$LANG2 Done!! "
+echo "Success !!! Two Experiment done"
